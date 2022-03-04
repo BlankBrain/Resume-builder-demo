2a3fa975de5c5d7e378561beb57b5379428966f7
Mehedi Hasan <m.mehedi_hasan@hotmail.com>
2022-03-04 06:54:48 +0600

    personal info page added

---
+++ Resume builder demo/PersonalInfoVc.swift
@@ -0,0 +1,21 @@
+//
+//  PersonalInfoVc.swift
+//  Resume builder demo
+//
+//  Created by Md. Mehedi Hasan on 4/3/22.
+//
+
+import UIKit
+
+class PersonalInfoVc: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    
+
+}
--- Resume builder demo/ViewController.swift
+++ Resume builder demo/ViewController.swift
@@ -9,11 +9,52 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var tableview: UITableView!
+    @IBOutlet weak var addResume: UIButton!
+    
+    var numberOfCellsTable:Int = 0
+    let cellIdentifiar : String = "cell"
+    
+    // MARK: TableView Vars
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        tableview.delegate = self
+        tableview.dataSource = self    }
+    
+    
+    // Button Action
+    @IBAction func addBtnClicked(_ sender: Any) {
+        performSegue(withIdentifier: "homeToPersonal", sender: self)
+        
     }
+    
+    
+    
+    
+    
 
 
 }
+extension ViewController:  UITableViewDelegate , UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifiar, for: indexPath)
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        print(indexPath.row)
+
+       }
+    
+    
+}
+
 
